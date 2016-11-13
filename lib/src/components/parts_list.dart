903332568a6bca6d24d58aa8d191507965ceae2a
Joel Trottier-Hebert <iamquebecois@gmail.com>
2016-11-13 16:40:48 -0500

    Added possibility to add photo for Parts

--- lib/src/components/parts_list.dart
+++ lib/src/components/parts_list.dart
@@ -1,10 +1,17 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:mouches/domain.dart';
+import 'image_dialog.dart';
 
 class PartsList extends StatelessWidget {
   List<Part> parts;
 
   PartsList(this.parts);
+
+  Future<dynamic> _showSimulationImageDialog(BuildContext c, Part p) =>
+      showDialog(
+          context: c, child: new ImageDialog(resourceUri: p.photo.resourceUri));
+
   @override
   Widget build(BuildContext context) {
     return new MaterialList(
@@ -12,10 +19,17 @@ class PartsList extends StatelessWidget {
             context: context,
             items: parts.map((Part part) {
               return new ListItem(
-                  title: new Text(part.name),
-                  subtitle: part.options.isEmpty
-                      ? null
-                      : new Text(part.options.join(', ')));
+                title: new Text(part.name),
+                subtitle: part.options.isEmpty
+                    ? null
+                    : new Text(part.options.join(', ')),
+                trailing: part.photo == null
+                    ? null
+                    : new IconButton(
+                        icon: new Icon(Icons.photo),
+                        onPressed: () =>
+                            _showSimulationImageDialog(context, part)),
+              );
             })));
   }
 }
